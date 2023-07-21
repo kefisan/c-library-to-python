66c81cbd87443ef4c00b07e886e7420d54aca962
uwu <interesofa@gmail.com>
2023-07-21 22:04:09 +0300

    reformatting

--- ui/window_ui.py
+++ ui/window_ui.py
@@ -5,6 +5,5 @@ root.option_add("*Font", "TkDefaultFont 24")
 root.title("Unit Calculator")
 root.geometry("910x350")
 
-# Text area
 label = tk.Label(text='Choose values:')
 label.grid(column=1, row=0, sticky='N')
